30334f3fe8dd69a9b9137d88e87c84babe8d2152
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2019-09-01 23:14:44 +0900

    first commit

---
+++ ydl.py
@@ -0,0 +1,33 @@
+import youtube_dl
+import sys
+import os
+import glob
+
+def dl(URL):
+	options = {
+		'format': 'bestaudio[ext=mp3]/bestaudio[ext=m4a]/bestaudio'
+	}
+	with youtube_dl.YoutubeDL(options) as ydl:
+		ydl.download([URL])
+
+def mp4to3(filename):
+	root, ext = os.path.splitext(filename)
+	if ext not in ['.m4a']: return
+	newname = '%s.mp3' % root
+	cmd = 'ffmpeg -i "%s" -ab 320k "%s"' % (filename, newname)
+	os.system(cmd)
+	os.remove(filename)
+
+def main(): 
+	format = input('フォーマット(mp3 or mp4): ')
+	url = input('URL: ')
+	if format == 'mp4': 
+		cmd = 'youtube-dl -f mp4 {URL}'.format(URL = url)
+		os.system(cmd)
+	else:
+		dl(url)
+		filenames = glob.glob('./*.m4a')
+		for filename in filenames:
+			mp4to3(filename)
+
+main()
