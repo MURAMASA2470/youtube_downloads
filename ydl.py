abf67451bd938484155e09915510e46caaffe7b8
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2021-07-04 19:59:43 +0900

    ffmpegでOSコマンドを使用しないように修正

--- ydl.py
+++ ydl.py
@@ -1,4 +1,5 @@
 import youtube_dl
+import ffmpeg
 import sys
 import os
 import glob
@@ -11,17 +12,18 @@ def dl(URL):
 		ydl.download([URL])
 
 def mp4to3(filename):
-	root, ext = os.path.splitext(filename)
-	if ext not in ['.m4a']: return
-	newname = '%s.mp3' % root
-	cmd = 'ffmpeg -i "%s" -ab 320k "%s"' % (filename, newname)
-	os.system(cmd)
-	os.remove(filename)
+  root, ext = os.path.splitext(filename)
+  if ext not in ['.m4a']: return
+  newname = '%s.mp3' % root
+  stream = ffmpeg.input(filename)
+  stream = ffmpeg.output(stream, newname, format='mp3', audio_bitrate=320)
+  ffmpeg.run(stream)
+  os.remove(filename)
 
-def main(): 
+def main():
 	format = input('Format(mp3 or mp4): ')
 	url = input('URL: ')
-	if format == 'mp4': 
+	if format == 'mp4':
 		cmd = 'youtube-dl -f mp4 {URL} --no-check-certificate'.format(URL = url)
 		os.system(cmd)
 	else:
