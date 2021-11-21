c7aa7863ad920866337ec635f13d60e078dbe726
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2021-11-21 20:35:05 +0900

    👍 色々改善

--- ydl.py
+++ ydl.py
@@ -4,17 +4,24 @@ import sys
 import os
 import glob
 import argparse
+import re
 
+# Download and convert
 def dl(url, format):
   options = {
-    # 'format': 'bestaudio[ext=mp3]/bestaudio[ext=m4a]/bestaudio',
     'nocheckcertificate:': True
 	}
   if format == 'mp3':
     options['format'] = 'bestaudio[ext=mp3]/bestaudio[ext=m4a]/bestaudio'
-  with youtube_dl.YoutubeDL(options) as ydl:
-    ydl.download([url])
+    youtube_dl.YoutubeDL(options).download([url])
 
+    filenames = glob.glob('./*.m4a')
+    for filename in filenames:
+      mp4to3(filename)
+  else:
+    youtube_dl.YoutubeDL(options).download([url])
+
+# Conver to mp3
 def mp4to3(filename):
   root, ext = os.path.splitext(filename)
   if ext not in ['.m4a']: return
@@ -24,36 +31,46 @@ def mp4to3(filename):
   ffmpeg.run(stream)
   os.remove(filename)
 
+# Batch download
 def dl4List(args):
   try:
     file = open(args.file, 'r', encoding='UTF-8')
     urls = file.readlines()
     for url in urls:
-      dl(url)
-      filenames = glob.glob('./*.m4a')
-      for filename in filenames:
-        mp4to3(filename)
+      dl(url, args.format)
     file.close()
   except FileNotFoundError as e:
     print('[Error] => File Not Found: ', e)
   finally:
     sys.exit()
 
+# validate to args
+def validateArgs(args):
+  err = None
+  if (args.format and args.format not in ['mp3', 'mp4']):
+      err = 'Incorrect format. Possible values are mp3 or mp4'
+  if (args.uri and not re.match('((https?)?://)?[^/]+/', args.uri)):
+      err = '\'{uri}\' is not a valid URL'.format(uri = args.uri)
+
+  if (err is not None):
+      print('[Error] => {error}'.format(error = err))
+      sys.exit()
+
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('-f', '--file', help='Please specify a text file for the URL list.')
+  parser.add_argument('-f', '--format', help='Select the file format (mp3 or mp4) to download.')
+  parser.add_argument('-u', '--uri', help='Specify the URI of the media to download.')
+  parser.add_argument('-l', '--list', help='Specify a text file for the URL list.')
   args = parser.parse_args()
-  if args.file: dl4List(args)
 
-  format = input('Format(mp3 or mp4): ')
-  url = input('URL: ')
-  if format == 'mp4':
-    cmd = 'youtube-dl -f mp4 {URL} --no-check-certificate'.format(URL = url)
-    os.system(cmd)
-  else:
-    dl(url)
-    filenames = glob.glob('./*.m4a')
-    for filename in filenames:
-      mp4to3(filename)
+  validateArgs(args)
+
+  if args.list:
+    dl4List(args)
+
+  format = args.format if (args.format) else input('Format(mp3 or mp4): ')
+  url = args.uri if (args.uri) else input('URL: ')
+
+  dl(url, format)
 
 main()
